b4f4519d76baa606d4f7dc26147efa8a83204af7
doruksayn <doruksayn@hotmail.com>
2026-01-09 04:42:42 +0300

    hw 9 vite connection and tasks done

---
+++ src/js/02-form.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+function loadFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const data = JSON.parse(savedData);
+    form.email.value = data.email || '';
+    form.message.value = data.message || '';
+  }
+}
+
+form.addEventListener('input', e => {
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.reset();
+});
+
+loadFormData();
